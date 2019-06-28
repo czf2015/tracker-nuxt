e25c48d37c02719cf9934542574310c2f2f3488e
czf2015 <122385930@qq.com>
2019-06-28 08:59:27 +0800

    文件迁移

--- utils/api.js
+++ utils/api.js
@@ -3,9 +3,5 @@ import axios from 'axios'
 export default axios.create({
     baseURL: '/api',
     timeout: 1000,
-    withCredentials: true
-    // proxy: {
-    //     host: '127.0.0.1',
-    //     port: 3000,
-    // }
+    withCredentials: true,
   });
